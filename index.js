54ea60c565025d4c291dd39146bce99c7048c2fa
Asheeqa Ali123 <shasha7880@gmail.com>
2024-04-04 22:32:42 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node 
+import inquire from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquire.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-10",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log('Congratulations! You guessed the right number.');
+}
+else {
+    console.log('You guessed the wrong number.');
+}
